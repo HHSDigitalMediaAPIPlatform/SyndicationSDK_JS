79438d76e3e90abecd55db41217a37559ba9b4b4
Ray Robertson <rrobertson@ctacorp.com>
2014-03-19 09:44:56 -0400

    1. Added in new API call methods. 2. Updated variables 3. Added comments

--- syndication-sdk.js
+++ syndication-sdk.js
@@ -1,12 +1,22 @@
 var Syndication = (function(){ 
 	
-	var _dataUrl = "http://ctacdev.com:8090/Syndication/api/v2/resources";
+	var _dataUrl = "http://ctacdev.com:8090/Syndication/api/v2";
 	var _thisObj = this; // Variable to map the 'Syndication' objects 'this'.
 	
-	var mediaSettings = {
+	/* Methods Paramers Objects */
+	/* Objects used for methods */
+	var getMediaParams = {
 		max: 18,
 		offset: 0
 	};
+
+	var getMediaEmbedParams = {
+		name: 'Syndicated Content'
+	}
+
+	var getMediaPreviewParams = {
+
+	}
 
 	var maxRecords = 18;
 	var pagination;
@@ -14,7 +24,10 @@ var Syndication = (function(){
 	apiCall = function( urlString, funcCallback, paramsObj) {
 		$.getJSON(urlString, paramsObj, function(data){
 			//console.log(data);
-			pagination = data.meta;
+			if(data.meta !== undefined){
+				pagination = data.meta.pagination;	
+			}
+			
 			funcCallback(data);
 		});
 	}
@@ -26,21 +39,21 @@ var Syndication = (function(){
 		},
 
 		/*
-			max,
-			offset,
-			sort,
-			order,
-			mediaType,
-			name,
-			nameContains,
-			descriptionContains,
-			sourceUrl,
-			sourceUrlContains,
-			dateContentAuthored,
-			contentAuthoredSinceDate,
-			contentAuthoredBeforeDate,
-			contentAuthoredInRange,
-			dateContentUpdated,
+			max 						: int
+			offset						: int
+			sourceUrlContains			: string
+			order						: string
+			mediaType 					: string
+			name 						: string
+			nameContains 				: string
+			descriptionContains			: string
+			sourceUrl 					: string
+			sourceUrlContains			: string
+			dateContentAuthored			: string
+			contentAuthoredSinceDate 	: string
+			contentAuthoredBeforeDate	: string
+			contentAuthoredInRange		: string
+			dateContentUpdated			: string
 			contentUpdatedSinceDate,
 			contentUpdatedBeforeDate,
 			contentUpdatededInRange,
@@ -78,31 +91,66 @@ var Syndication = (function(){
 			restrictToSet
 		*/
 
-		getAllMediaTypes : function(callback, options) {
+		/*GET API media.json CALL*/ 
+		// Was 'getAllMediaTypes' no is 'getMedia'
+		getMedia : function(callback, options) {
 
-	        var requestOptions = $.extend( {}, mediaSettings, options );
+	        var getMediaOptions = $.extend( {}, getMediaParams, options );
 
 			
-			var results = _thisObj.apiCall(_dataUrl + '/media.json?callback=?', function(data){
+			var results = _thisObj.apiCall(_dataUrl + '/resources/media.json?callback=?', function(data){
 				callback(data.results);
-			}, requestOptions);
+			}, getMediaOptions);
 
 		},
 
-		getPage : function(url, callback) {
-			
-			_thisObj.apiCall(url, function(data) {
+		/*GET API {id}/alternateImages.json CALL*/
+		getMediaAlternateImagesById: function(id, callback){
+			_thisObj.apiCall(_dataUrl + '/resources/media/'+id.toString()+'/alternateImages.json?callback=?', function(data){
 				callback(data.results);
 			});
-
 		},
 
-		getMediaMetadataByMediaId : function() {
-
+		/*GET API media/{id}.json CALL*/
+		getMediaById: function(id, callback){
+			_thisObj.apiCall(_dataUrl + '/resources/media/'+id.toString()+'.json?callback=?', function(data){
+				callback(data.results);
+			});
 		},
 
-		getMediaPreviewByMediaId : function(id) {
-		return _dataUrl + '/media/' + id.toString() + '/preview.jpg?previewSize=custom&width=570&height=400&crop=true';
+		/*GET API media/{id}/content CALL*/
+		getMediaContentById: function(id, callback){
+			_thisObj.apiCall(_dataUrl + '/resources/media/'+id.toString()+'/content?callback=?', function(data){
+				callback(data);
+			});
+		},
+
+		/**/
+		getMediaEmbedById: function(id, callback, options){
+
+			var getMediaEmbedOptions = $.extend( {}, getMediaEmbedParams, options );
+			console.log("====> "+_dataUrl + '/resources/media/'+id.toString()+'/embed.html?callback=?');
+			var results = _thisObj.apiCall(_dataUrl + '/resources/media/'+id.toString()+'/embed.html?callback=?', function(data){
+				callback(data);
+			}, getMediaEmbedOptions);
+		},
+
+		/**/
+		getMediaHtmlById : function(id, callback, options) {
+			var getMediaHtmlOptions = $.extend( {}, options );
+
+			var results = _thisObj.apiCall(_dataUrl + '/resources/media/'+id.toString()+'/syndicate.json?callback=?', function(data){
+				callback(data);
+			}, getMediaHtmlOptions);
+		},
+
+		/*GET API {id}/preview.json CALL*/
+		getMediaPreviewById : function(id, options) {
+			var getMediaPreviewOptions = $.extend( {}, getMediaPreviewParams, options );
+			console.log(getMediaPreviewOptions.imageFloat);
+			var append;
+			return _dataUrl + '/resources/media/' + id.toString() + '/preview.jpg?'+getMediaPreviewOptions;
+			//previewSize=custom&width=570&height=400&crop=true';
 		},
 
 		searchMediaMetadata : function(searchfield, callback) {
@@ -113,7 +161,7 @@ var Syndication = (function(){
 			var searchField = searchfield.val();
 			var syndicatedSearchArray = [];
 			var syndicatedSearchIdsArray = [];
-			var searchQuery = _thisObj.apiCall(_dataUrl + '.json?q=' + escape(searchField) + '&callback=?', function(data){
+			var searchQuery = _thisObj.apiCall(_dataUrl + '/resources.json?q=' + escape(searchField) + '&callback=?', function(data){
 				
 				syndicatedSearchArray = data.results[0];
 
@@ -125,13 +173,22 @@ var Syndication = (function(){
 
 				});
 
-				_thisObj.apiCall(_dataUrl + '/media.json?restrictToSet=' + syndicatedSearchIdsArray.join() + '&callback=?', function(data){
+				_thisObj.apiCall(_dataUrl + '/resources/media.json?restrictToSet=' + syndicatedSearchIdsArray.join() + '&callback=?', function(data){
 					callback(data.results);
 				});
 
 			});
 			
-		}		
+		},
+
+		/* HELPER FUNCTION */
+		getPage : function(url, callback) {
+			
+			_thisObj.apiCall(url, function(data) {
+				callback(data.results);
+			});
+
+		}				
 
 	}
 }());
